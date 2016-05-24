33426c70882ebf40d252429aee918ccd9c8da580
Matthew Potter <matthew.potter@integritystl.com>
2016-05-24 11:23:21 -0500

    patchin deploy

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ require('dotenv').config();
 const config = require('./config');
 //initialize bots
 require('./src/mathbear')(config.bots.mathbear);
-
-
 require('./src/wonder')(config.bots.botOfWonder);
