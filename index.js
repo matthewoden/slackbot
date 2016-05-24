e92a8ff0992243fb8c08ad39e8a3604cc4d1ca4a
Matthew Potter <matthew.potter@integritystl.com>
2016-05-24 10:11:15 -0500

    testing automated restart

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 //load environment variables
 require('dotenv').config();
+
 const config = require('./config');
 //initialize bots
 require('./src/mathbear')(config.bots.mathbear);
