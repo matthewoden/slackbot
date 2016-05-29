b0e52303b4cb1e239c2550d9cbd68e5e80dfb488
Matthew Potter <matthew.potter@integritystl.com>
2016-05-29 13:19:21 -0500

    testing webhook sorcery

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-//load environment variables
+//load environment variablespf
 require('dotenv').config();
 const config = require('./config');
-
 //initialize bots
 require('./src/mathbear')(config.bots.mathbear);
 require('./src/wonder')(config.bots.botOfWonder);
