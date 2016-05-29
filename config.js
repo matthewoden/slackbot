f2f72ec3db63c92878316135c0cb480a8eac80e8
Matthew Potter <matthew.potter@integritystl.com>
2016-05-29 09:05:26 -0500

    testing webhook sorcery.

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
-// var config = {
-//   "BOTS": [{
-//     "SLACK_API_TOKEN": "xoxb-44897533749-obLj855ediYvzzfOfKZjbQKI",
-//     "SLACK_BOT_NAME": "mathbear",
-//     "SLACK_BOT_ID": "<@U1ASDFPN1>"
-//     }, {
-//     "SLACK_API_TOKEN": "xoxb-44897183238-ky9llHluky8nEkRodbHV3a17",
-//     "SLACK_BOT_NAME": "botofwonder",
-//     "SLACK_BOT_ID": "<@U1ASD5D70>"
-//     }
-//   ]
-// }
-
 module.exports = {
   bots:{
     botOfWonder:{
